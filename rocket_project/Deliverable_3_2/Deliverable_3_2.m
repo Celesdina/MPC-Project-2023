0ff6c9232dae1d70a5a4e1f30c3aca23cc294504
Le0wl <leonie.gasser@epfl.ch>
2023-12-29 16:08:01 +0100

    x works but idk why

--- rocket_project/Deliverable_3_2/Deliverable_3_2.m
+++ rocket_project/Deliverable_3_2/Deliverable_3_2.m
@@ -1,7 +1,41 @@
 addpath(fullfile('..', 'src'));
 
-%close all
-%clear all
-%clc
+addpath(fullfile('..', '@Rocket'));
+addpath("C:\Users\leoga\Documents\MATLAB\MPC\programms\casadi-3.6.4-windows64-matlab2018b");
+
+close all
+clear all
+clc
 
 %% TODO: This file should produce all the plots for the deliverable
+
+Ts = 1/20; % Sample time
+rocket = Rocket(Ts);
+[xs, us] = rocket.trim();
+sys = rocket.linearize(xs, us);
+
+[sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us);
+
+%% Design MPC controller x
+% subsys 1: sys_x 
+% states: x_x = (wy, beta, vx, x)       input: u_x = (d2)
+
+H = 30; % Horizon length in seconds
+mpc_x = MpcControl_x(sys_x, Ts, H);
+
+% contorl input
+x0_x = [0;0;0;0]; % we start at the origin
+ref_x = [-4]; % we want to go to x=-4
+
+% %closed loop 
+Tf = 10;
+
+[T, X_sub, U_sub] = rocket.simulate_f(sys_x, x0_x, Tf, @mpc_x.get_u, ref_x);
+ph = rocket.plotvis_sub(T, X_sub, U_sub, sys_x, xs, us, ref_x);
+
+
+
+%% Design MPC controller y
+% subsys 2: sys_y
+% states: x_y = (wx, alpha, vy, y)      input: u_y = (d1)
+
