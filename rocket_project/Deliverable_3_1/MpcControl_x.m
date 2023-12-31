10eab580a8c388c6fb25fdf6f191622d98976839
Celesdina <dialonlyhime@gmail.com>
2023-12-31 13:09:30 +0100

    3_1 put together

--- rocket_project/Deliverable_3_1/MpcControl_x.m
+++ rocket_project/Deliverable_3_1/MpcControl_x.m
@@ -1,4 +1,4 @@
-classdef MpcControl_roll < MpcControlBase
+classdef MpcControl_x < MpcControlBase
     
     methods
         % Design a YALMIP optimizer object that takes a steady-state state
@@ -11,61 +11,92 @@ classdef MpcControl_roll < MpcControlBase
             %   x_ref, u_ref - reference state/input
             % OUTPUTS
             %   U(:,1)       - input to apply to the system
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             
             N_segs = ceil(H/Ts); % Horizon steps
             N = N_segs + 1;      % Last index in 1-based Matlab indexing
+
+            [nx, nu] = size(mpc.B);
             
-            [nx, nu] = size(mpc.B); %(2x1)
-            
-            % Steady-state targets (Ignore this before Todo 3.2)
-            x_ref = sdpvar(nx, 1); % (2x1)
-            u_ref = sdpvar(nu, 1);  % (1x1)
+            % Targets (Ignore this before Todo 3.2)
+            x_ref = sdpvar(nx, 1);
+            u_ref = sdpvar(nu, 1);
             
             % Predicted state and input trajectories
-            X = sdpvar(nx, N); % (2,N)
-            U = sdpvar(nu, N-1); % (1,N-1)
+            X = sdpvar(nx, N);
+            U = sdpvar(nu, N-1);
             
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
             
             % NOTE: The matrices mpc.A, mpc.B, mpc.C and mpc.D are
             %       the DISCRETE-TIME MODEL of your system
+
+            % subsys 1: sys_x 
+            % states: x_x = (wy, beta, vx, x)       input: u_x = (d2)
+            % Constraints : 
+            % u in U = { u | Mu <= m }
+            M = [1;-1]; m = [0.26; 0.26];
+            % x in X = { x | Fx <= f }
+            F = [1 0 0 0; 0 1 0 0; 
+                0 0 1 0; 0 0 0 1;
+                -1 0 0 0; 0 -1 0 0; 
+                0 0 -1 0; 0 0 0 -1]; 
+            f = [100; 0.1745; 100; 100; 100; 0.1745; 100; 100];
             
+            Q = 1 * eye(4);
+            R = 1;
+            
+            % Compute LQR controller for unconstrained system
+            [K,Qf,~] = dlqr(mpc.A,mpc.B,Q,R);
+            % MATLAB defines K as -K, so invert its signal
+            K = -K; 
+            
+            % Compute maximal invariant set
+            Xf = polytope([F;M*K],[f;m]);
+            Acl = [mpc.A+mpc.B*K];
+            while 1
+                prevXf = Xf;
+                [T,t] = double(Xf);
+                preXf = polytope(T*Acl,t);
+                Xf = intersect(Xf, preXf);
+                if isequal(prevXf, Xf)
+                    break
+                end
+            end
+            [Ff,ff] = double(Xf);
+            
+            % % MPT version
+            sys = LTISystem('A',mpc.A,'B',mpc.B);
+            sys.x.min = [-100; -0.1745; -100; -100]; sys.x.max = [100; 0.1745; 100; 100];
+            sys.u.min = [-0.26]; sys.u.max = [0.26];
+            sys.x.penalty = QuadFunction(Q); sys.u.penalty = QuadFunction(R);
+
+            Xf_mpt = sys.LQRSet;
+            Qf_mpt = sys.LQRPenalty;
+            
+            tiledlayout(1, 3);
+            nexttile;
+            Xf_mpt.projection(1:2).plot();
+            title('terminal set wy and beta');
+            nexttile;
+            Xf_mpt.projection(2:3).plot();
+            title('terminal set beta and vx');
+            nexttile;
+            Xf_mpt.projection(3:4).plot();            
+            title('terminal set vx and x');
+
             % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj HERE
-            %Pdiff +-20% 
-            Hu = [1;-1]; hu = [20;20]; 
-            
-            %LQR
-            Q=200*eye(nx); 
-            R=0.2*eye(nu);
+            obj = 0;
+            con = (X(:,2) == mpc.A*X(:,1) + mpc.B*U(:,1)) + (M*U(:,1) <= m);
 
-            sys = LTISystem('A', mpc.A, 'B', mpc.B);
-            sys.u.min = -20; sys.u.max= 20;
-            sys.x.penalty= QuadFunction(Q); sys.u.penalty=QuadFunction(R);
-
-            Xf=sys.LQRSet;
-            Qf=sys.LQRPenalty.H;
-            [Ff,ff]=double(polytope(Xf));
-            
-            % plot terminal invariant set
-            figure
-            hold on; grid on;
-            plot(polytope(Xf),'r');
-            title("Terminal set for roll")
-            
-            % constraints
-            obj = U(:,1)'*R*U(:,1);
-            con = (X(:,2) == mpc.A*X(:,1)+mpc.B*U(:,1))+(Hu * U(:, 1) <= hu);
             for i = 2:N-1
                 con = con + (X(:,i+1) == mpc.A*X(:,i) + mpc.B*U(:,i));
-                con = con + (Hu*U(:,i) <= hu);
+                con = con + (F*X(:,i) <= f) + (M*U(:,i) <= m); 
                 obj = obj + X(:,i)'*Q*X(:,i) + U(:,i)'*R*U(:,i);
             end
-            con = con + (Ff*X(:,N) <= ff);
+            con = con + (Ff*X(:,N) <= ff);    
             obj = obj + X(:,N)'*Qf*X(:,N);
-            
-            
             % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             
@@ -85,8 +116,9 @@ classdef MpcControl_roll < MpcControlBase
             %   xs, us - steady-state target
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             
-            % Steady-state targets
             nx = size(mpc.A, 1);
+
+            % Steady-state targets
             xs = sdpvar(nx, 1);
             us = sdpvar;
             
