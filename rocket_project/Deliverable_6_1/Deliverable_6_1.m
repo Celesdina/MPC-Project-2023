538ad959c2f968d0d1a9b5fd1bd562cfa892255f
Le0wl <leonie.gasser@epfl.ch>
2024-01-08 15:30:41 +0100

    6.1 now makes a graph at least

--- rocket_project/Deliverable_6_1/Deliverable_6_1.m
+++ rocket_project/Deliverable_6_1/Deliverable_6_1.m
@@ -1,7 +1,37 @@
 addpath(fullfile('..', 'src'));
+addpath(fullfile('..', '@Rocket'));
+addpath("C:\Users\leoga\Documents\MATLAB\MPC\programms\casadi-3.6.4-windows64-matlab2018b");
+import casadi.*
+
 
 %close all
 %clear all
 %clc
 
 %% TODO: This file should produce all the plots for the deliverable
+Ts = 1/20;
+rocket = Rocket(Ts);
+
+H = 5; % Horizon length in seconds
+nmpc = NmpcControl(rocket, H);
+
+% MPC reference with default maximum roll = 15 deg
+ref = @(t_, x_) ref_TVC(t_);
+% MPC reference with specified maximum roll = 50 deg
+% roll_max = deg2rad(50);
+% ref = @(t_, x_) ref_TVC(t_, roll_max);
+% Evaluate once and plot optimal open−loop trajectory,
+% pad last input to get consistent size with time and state
+
+x1 = zeros(12,1);
+ref4 = [10 10 10 deg2rad(15)]';
+
+[u, T_opt, X_opt, U_opt] = nmpc.get_u(x1, ref4);
+U_opt(:,end+1) = nan;
+ph = rocket.plotvis(T_opt, X_opt, U_opt, ref4);
+title('open loop');
+
+Tf = 30;
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
+% ph = rocket.plotvis(T, X, U, Ref);
+% title('closed loop');
